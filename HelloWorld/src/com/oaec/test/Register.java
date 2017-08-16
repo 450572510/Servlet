df1f095cda9afeda0587aa559fadd27c9a2f5f4d
yfg <18763718132@163.com>
2017-08-16 22:57:54 +0800

    获取表单数据

--- HelloWorld/src/com/oaec/test/Register.java
+++ HelloWorld/src/com/oaec/test/Register.java
@@ -1,32 +1,47 @@
 package com.oaec.test;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/*
+ * 读取表单数据
+ */
+// 1.创建 extends 的 Httpservlet ↓
 public class Register extends HttpServlet {
 
+	// 2. doGet方法   ↓ 
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-
+		req.setCharacterEncoding("utf-8");
+		String name = req.getParameter("username");
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter w = resp.getWriter();
+		w.write(name);
+		w.close();
 	}
 
+	// 3. doPost 方法   ↓
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
 		String sex = req.getParameter("sex");
-		// 1).多选项用 req.getParameterValues 拿到的是数组形式的返回值
-		req.getParameterValues("");
-		// 然后遍历上面那一步括号中的值
-		for (String s : 上面括号内的值) {
-			System.out.println(s);
-		}
 
+		resp.setCharacterEncoding("utf-8");
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter writer = resp.getWriter();
+		writer.write("用户名<span style='color:red'>" + username + "</span><br/>"
+				+ "密码<span style='color:red'>" + password + "</span><br/>");
+		writer.close();
 	}
 }
